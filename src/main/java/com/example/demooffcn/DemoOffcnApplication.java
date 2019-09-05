9253f49a830b1ddf37428dd787d4d42eee3985c2
cwj67422 <cwj67422@163.com>
2019-09-05 22:49:51 +0800

    提交demon-offcn项目

---
+++ src/main/java/com/example/demooffcn/DemoOffcnApplication.java
@@ -0,0 +1,13 @@
+package com.example.demooffcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOffcnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOffcnApplication.class, args);
+    }
+
+}
